1bbadedd5e33d857cf7660ae3dfc24db1ee22f96
jun118166 <zhijun916@163.com>
2016-10-30 22:36:04 +0800

    优化

--- js/base.js
+++ js/base.js
@@ -1,21 +1,6 @@
 /*
- * 提供一些基础方法 与语言 结构相关性较大 与业务逻辑相关性较小 
+ * 获取页面参数
  */
-/*
- * 命名空间
- */
-var IU = {};
-IU.namespace = function(str) {
-		var arr = str.split("."),
-			o = IU;
-		for(i = (arr[0] == "IU") ? 1 : 0; i < arr.length; i++) {
-			o[arr[i]] = o[arr[i]] || {};
-			o = o[arr[i]];
-		}
-	}
-	/*
-	 * 获取页面参数
-	 */
 function geturlparam(name) {
 	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
 	var r = window.location.search.substr(1).match(reg); //匹配目标参数
@@ -37,19 +22,18 @@ function jump(url) {
 		location.href = location.origin + pathname + url;
 	}
 };
-var host = 'http://localhost:3536/api/';
-
-function getData(param, sucFunc) {
-	if($.isArray(param)) param = param.join('/');
-	$.ajax({
-		type: "get",
-		url: host + param,
-		async: true,
-		success: function(data) {
-			sucFunc(data);
-		},
-		err: function(e) {
-
-		}
-	});
+/*
+ * 提供一些基础方法 与语言 结构相关性较大 与业务逻辑相关性较小 
+ */
+/*
+ * 命名空间
+ */
+var IU = {};
+IU.namespace = function(str) {
+	var arr = str.split("."),
+		o = IU;
+	for(i = (arr[0] == "IU") ? 1 : 0; i < arr.length; i++) {
+		o[arr[i]] = o[arr[i]] || {};
+		o = o[arr[i]];
+	}
 }
