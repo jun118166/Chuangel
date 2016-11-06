60d930144e0ae0300198099940b4eeb5a204e1d1
jun118166 <zhijun916@163.com>
2016-11-06 22:35:36 +0800

    优化

---
+++ js/store.data.js
@@ -0,0 +1,26 @@
+var cityData = [{
+	value: '1',
+	text: '玄武区',
+	children: [{
+		value: "10",
+		text: "玄武区169号三楼201"
+	}]
+}, {
+	value:'2',
+	text: '秦淮区'
+},{
+	value:'3',
+	text: '鼓楼区'
+},{
+	value:'4',
+	text: '栖霞区'
+},{
+	value:'5',
+	text: '江宁区'
+},{
+	value:'6',
+	text: '秦淮区'
+},{
+	value:'7',
+	text: '雨花台区'
+}];
